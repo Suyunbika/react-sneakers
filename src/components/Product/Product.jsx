5783895f46b4319469e9b5ce8d1945e4f1a09e26
bikmullina <bikmullina0703@mail.ru>
2023-02-06 15:04:33 +0300

    Add modal

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -1,23 +1,23 @@
+import formatMoney from "../../utils/formatMoney";
 import productImage from "../../assets/product_1.png";
 import { HeartIcon, PlusIcon } from "../Icons/Icons";
 
-const Product = () => {
+const Product = ({ product }) => {
     return (
         <div className="product">
             <div className="product__action">
                 <HeartIcon />
             </div>
 
-            <img src={productImage} alt="" />
+            <img src={product.preview} alt={product.name} />
 
-            <h3>Наименование продукта</h3>
+            <h3>{product.name}</h3>
 
             <div className="product__footer">
 
                 <div className="price">
                     <span className="gray">Цена:</span>
-
-                    <span className="value">12 999 руб.</span>
+                    <span className="value">{formatMoney(product.price)}</span>
                 </div>
 
                 <button>
